361ebfac38499db60a7f47c9f39f139a7d7f5c73
Dan Gastler <dgastler@gmail.com>
2021-06-15 12:11:45 -0400

    More Ubutnu fixes

--- env.sh
+++ env.sh
@@ -4,9 +4,9 @@ source env.sh
 cd ..
 #setup BUTOOL_AUTOLOAD_LIBRARY_LIST
 #must be source AFTER build finishes
-plugin_paths=$(find $PWD/plugins/*/lib/*evice.so -type f | xargs)
+plugin_paths=$(find $PWD/plugins/*/ -type f | xargs)
 for path in $plugin_paths
 do
-    export BUTOOL_AUTOLOAD_LIBRARY_LIST=$path:$BUTOOL_AUTOLOAD_LIBRARY_LIST
+    export BUTOOL_AUTOLOAD_LIBRARY_LIST=$path/lib:$BUTOOL_AUTOLOAD_LIBRARY_LIST
 done
   
